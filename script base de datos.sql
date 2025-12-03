2e0df3cdaa6c14233a7d286a4bceb32a724efae1
SHW378 <cesarbrmn@hotmail.com>
2025-12-03 15:08:45 -0600

    sql

---
+++ script base de datos.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE IF NOT EXISTS inventario_db;
+USE inventario_db;
+
+CREATE TABLE categorias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL UNIQUE
+);
+
+INSERT INTO categorias (nombre) VALUES ('Electrónica'), ('Muebles'), ('Accesorios'), ('Ropa'), ('Hogar');
+insert into categorias (nombre) values ('Perifericos');
+
+CREATE TABLE productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sku VARCHAR(50) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL DEFAULT 0,
+    categoria_id INT NOT NULL, -- Relación con categorías
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+CREATE TABLE movimientos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    producto_id INT NOT NULL,
+    tipo ENUM('Entrada', 'Salida') NOT NULL,
+    cantidad INT NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (producto_id) REFERENCES productos(id) ON DELETE CASCADE
+);
+
+INSERT INTO productos (sku, nombre, precio, stock, categoria_id) VALUES 
+('LAP-001', 'Laptop HP', 1200.00, 10, 1), 
+('SILLA-X', 'Silla Gamer', 250.00, 5, 2); 
+
+
+
+select * from productos
